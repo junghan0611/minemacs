08aab04ce23195ef1b8589e3245bf0c24d4de39b
Junghan Kim <******+github-account@users.noreply.github.com>
2023-01-19 16:05:08 +0900

    add me-org-ko.el

--- config.el
+++ config.el
@@ -2,60 +2,97 @@
 
 ;; Copyright (C) 2022 Abdelhak Bougouffa
 
-(load-path "~/.spacemacs.d/lisp/")
+(message "MY-MINEMACS-KR-LOADING... START")
+(add-to-list 'load-path "~/.spacemacs.d/lisp/")
+
+(use-package unfill :straight t)
+(use-package winum :straight t)
 
 ;; Personal info
 (setq user-full-name "Junghan Kim"
       user-mail-address "junghanacs@gmail.com")
-
+(setq auth-sources '("~/.authinfo.gpg"))
 ;; Set the default GPG key ID, see "gpg --list-secret-keys"
 (setq-default epa-file-encrypt-to '("B5ADD9F47612A9DB"))
 
-(setq utf-translate-cjk-mode nil)       ; disable CJK coding/encoding
+;; Line should be 80 characters wide, not 72 from corgi emacs
+(setq-default
+ ;; ====== Buffer-local variables ======
+ ;; Display long lines
+ truncate-lines nil
+ ;; Default fill column width
+ fill-column 80
+ ;; Never mix, use only spaces
+ indent-tabs-mode nil ;; Width for line numbers display-line-numbers-width 4
+ ;; Small tab is enough!
+ tab-width 2)
+(setq inhibit-compacting-font-caches t)
 
+;; Easily navigate sillycased words
+(global-subword-mode 1)
+;; Stop asking abount following symlinks to version controlled files
+(setq vc-follow-symlinks t)
+
+;; read 'The Forgotten History of the Blinking Cursor'
+(blink-cursor-mode 1)
+
+;; Disable .# lock files
+(setq create-lockfiles nil)
+
+;; Shr group: Simple HTML Renderer를 의미한다. 여기 설정을 바꾸면 faces를 수정할 수 있음
+(setq shr-use-fonts nil)
+
+;; UTF-8 :: defined in corgi-defaults.el
 (setq default-input-method "korean-hangul")
 (set-language-environment "Korean")
 (set-keyboard-coding-system 'utf-8)
-(setq locale-coding-system  'utf-8)
+(setq locale-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
-(setq-default buffer-file-coding-system 'utf-8-unix)
 (prefer-coding-system 'utf-8)
+(setq-default buffer-file-coding-system 'utf-8-unix)
 
-(global-set-key (kbd "<S-SPC>") 'toggle-input-method)
-;; (global-set-key (kbd "<Alt_R>") 'toggle-input-method)
 (global-set-key (kbd "<Hangul>") 'toggle-input-method)
-;; (global-unset-key (kbd "S-SPC"))
+(global-set-key (kbd "<S-SPC>") 'toggle-input-method)
 
+(setq system-time-locale "C")
+
+;; +------------+------------+
+;; | 일이삼사오 | 일이삼사오 |
+;; +------------+------------+
+;; | ABCDEFGHIJ | ABCDEFGHIJ |
+;; +------------+------------+
+;; | 1234567890 | 1234567890 |
+;; +------------+------------+
+;; | 일이삼사오 | 일이삼사오 |
+;; | abcdefghij | abcdefghij |
+;; +------------+------------+
+
+(defun my/korean-set-font-face()
+  (interactive)
+  (set-face-font 'fixed-pitch "Sarasa Term K")
+  (set-face-font 'fixed-pitch-serif "KoPubWorldBatang")
+  (set-face-font 'variable-pitch "Source Han Sans K VF")
+  (set-face-attribute 'default nil :family "Sarasa Mono K" :height 136)
+  (set-fontset-font t 'hangul (font-spec :family "Sarasa Mono K" :spacing 'm :size 13.5 :avgwidth 13.5)))
+
+(add-hook 'minemacs-after-startup-hook 'my/korean-set-font-face)
+(add-hook 'after-init-hook 'my/korean-set-font-face)
+
+;; (add-hook 'enable-theme-functions 'my/korean-set-font-face)
 ;; Set fonts
 (setq minemacs-fonts ;; or Cascadia Code, Fira Code, FiraCode Nerd Font, Iosevka Fixed Curly Slab
-      '(:font-family "Sarasa Mono K Nerd Font" ;; "Iosevka Fixed Curly Slab"
+      '(:font-family "Sarasa Mono K" ;; "Iosevka Fixed Curly Slab"
         :font-size 13.5
         :variable-pitch-font-family "Source Han Sans K VF"
         :variable-pitch-font-size 13.5))
 
 (setq-default line-spacing 3)
-
 ;; (setq minemacs-theme 'doom-homage-white)
 
-;;  *한영 자간 확인*
-;;  +------------+------------+
-;;  | 일이삼사오 | 일이삼사오 |
-;;  | abcdefghij | abcdefghij |
-;;  +------------+------------+)
-
-(defun my/korean-set-font-face()
-  (interactive)
-  (set-face-font 'fixed-pitch "Sarasa Mono K Nerd Font")
-  (set-face-font 'fixed-pitch-serif "KoPubWorldBatang")
-  (set-face-font 'variable-pitch "Source Han Sans K VF")
-  (set-face-attribute 'default nil :family "Sarasa Mono K Nerd Font" :height 136)
-  (set-fontset-font t 'hangul (font-spec :family "Sarasa Mono K Nerd Font" :spacing 'm :size 13.5 :avgwidth 13.5)))
-
-(add-hook 'minemacs-after-startup-hook 'my/korean-set-font-face)
-(add-hook 'after-init-hook 'my/korean-set-font-face)
-;; (add-hook 'enable-theme-functions 'my/korean-set-font-face)
+;; (require 'corgi-stateline)
+;; (global-corgi-stateline-mode)
 
 ;; If you installed Emacs from source, you can add the source code
 ;; directory to enable jumping to symbols defined in Emacs' C code.
@@ -63,6 +100,7 @@
 
 ;; I use Brave, and never use Chrome, so I replace chrome program with "brave"
 ;; (setq browse-url-chrome-program (or (executable-find "brave") (executable-find "chromium")))
+
 
 ;; Module: `me-natural-langs' -- Package: `spell-fu'
 (with-eval-after-load 'spell-fu
@@ -118,18 +156,113 @@
 ;;                                 "Lastname" ;; Last name
 ;;                                 "/R&D Engineer at Some company/")))))
 
+;; 편집모드에서 빠져나갈때 한글이 켜져있으면 이후에 노멀 모드에서 커맨드가 안먹히는 짜증이 발생한다.
+;; 그냥 편집모드에서 빠져나갈때 무조건 리셋시킨다. 엄청 편리함
+(add-hook 'evil-insert-state-exit-hook (lambda ()
+                                         (setq evil-input-method nil)))
+;;; EVIL
+;; Replace Emacs Tabs key bindings with Workspace key bindings
+  (with-eval-after-load 'evil-maps ;; 편집 창 포커스 이동을 간단하게
+    (define-key evil-normal-state-map (kbd "<SPC> <right> ") 'evil-window-right)
+    (define-key evil-normal-state-map (kbd "<SPC> <left> ") 'evil-window-left)
+    (define-key evil-normal-state-map (kbd "<SPC> <up> ") 'evil-window-up)
+    (define-key evil-normal-state-map (kbd "<SPC> <down> ") 'evil-window-down)
+
+
+    ;; disable evil-mc key-binding with meta
+    ;; (with-eval-after-load 'evil-mc
+    ;;   (evil-define-key '(insert normal visual) evil-mc-key-map (kbd "M-n") nil)
+    ;;   (evil-define-key '(insert normal visual) evil-mc-key-map (kbd "M-p") nil)
+    ;;   (evil-define-key '(normal visual) evil-mc-key-map (kbd "C-n") nil)
+    ;;   (evil-define-key '(normal visual) evil-mc-key-map (kbd "C-p") nil)
+    ;;   (evil-define-key '(normal visual) evil-mc-key-map (kbd "C-t") nil)
+    ;;   (evil-define-key '(normal visual) evil-mc-key-map (kbd "C-M-j") nil)
+    ;;   (evil-define-key '(normal visual) evil-mc-key-map (kbd "C-M-k") nil)
+    ;;   )
+
+    ;; overide better-defaults layer configuration
+    (define-key evil-normal-state-map (kbd "C-a") 'evil-beginning-of-line)
+    (define-key evil-normal-state-map (kbd "C-e") 'evil-end-of-line-or-visual-line)
+    (define-key evil-insert-state-map (kbd "C-a") 'evil-beginning-of-line)
+    (define-key evil-insert-state-map (kbd "C-e") 'evil-end-of-line-or-visual-line)
+    ;; =C-w= 'insert 'evil-delete-backward-word
+    ;; =C-w= 'visual 'evil-window-map
+
+    (global-set-key (kbd "M-q") 'fill-paragraph)
+    (global-set-key (kbd "C-M-q") 'unfill-paragraph)
+
+    ;; copy from DW
+    (defun dw/dont-arrow-me-bro ()
+      (interactive)
+      (message "Arrow keys are bad, you know?"))
+
+    ;; Disable arrow keys in normal and visual modes
+    (define-key evil-normal-state-map (kbd "<left>") 'dw/dont-arrow-me-bro)
+    (define-key evil-normal-state-map (kbd "<right>") 'dw/dont-arrow-me-bro)
+    (define-key evil-normal-state-map (kbd "<down>") 'dw/dont-arrow-me-bro)
+    (define-key evil-normal-state-map (kbd "<up>") 'dw/dont-arrow-me-bro)
+    (evil-global-set-key 'motion (kbd "<left>") 'dw/dont-arrow-me-bro)
+    (evil-global-set-key 'motion (kbd "<right>") 'dw/dont-arrow-me-bro)
+    (evil-global-set-key 'motion (kbd "<down>") 'dw/dont-arrow-me-bro)
+    (evil-global-set-key 'motion (kbd "<up>") 'dw/dont-arrow-me-bro)
+
+    ;; C-g 기본 함수는 keyboard-quit
+    ;; C-g back to normal state
+    ;; (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+    )
+;; windows 1-6
+(global-set-key (kbd "M-1") 'winum-select-window-1)
+(global-set-key (kbd "M-2") 'winum-select-window-2)
+(global-set-key (kbd "M-3") 'winum-select-window-3)
+(global-set-key (kbd "M-4") 'winum-select-window-4)
+(global-set-key (kbd "M-5") 'winum-select-window-5)
+(global-set-key (kbd "M-6") 'winum-select-window-6)
+
+;; show major-mode keymap
+(global-set-key (kbd "M-<RET>") 'which-key-show-major-mode)
+;; (global-set-key (kbd "M-<RET>") 'cider-eval-last-sexp)
+
+;; Info 모드 Node 이동
+;; (define-key Info-mode-map (kbd "C-p") 'Info-prev)
+;; (define-key Info-mode-map (kbd "C-n") 'Info-next)
+
+;; undo-fu
+(define-key evil-normal-state-map (kbd "u") 'undo-fu-only-undo)
+(define-key evil-normal-state-map (kbd "\C-r") 'undo-fu-only-redo)
+
+(global-set-key (kbd "C-c j b") 'org-babel-tangle)
+(global-set-key (kbd "C-c j j") 'org-journal-new-entry)
+
+;; (setq multi-term-program "/usr/bin/zsh")
+
+(global-visual-line-mode 1)
+
+(with-eval-after-load 'evil-escape
+        (setq-default evil-escape-key-sequence ",.")
+        (setq-default evil-escape-unordered-key-sequence nil)
+        (setq-default evil-escape-delay 1.0) ;; 0.5, default 0.1
+        (evil-escape-mode)
+        )
+
+(with-eval-after-load 'vertico
+  (evil-global-set-key 'normal "." 'consult-line)
+  )
+
 (message "MY-WORKFLOW-KR-LOADING... START")
 
 ;; Module: `me-org' -- Package: `org'
 (with-eval-after-load 'org
   ;; Set Org-mode directory
-  (use-package 'org-journal
+  (use-package org-agenda
+    :straight (:type built-in))
+
+  (use-package org-journal
     :straight t
     )
-  (use-package 'bbdb
+  (use-package bbdb
     :straight t
     )
-  (use-package 'org-pomodoro
+  (use-package org-pomodoro
     :straight t
     )
 
@@ -137,14 +270,14 @@
   (require 'org-workflow)
 
   ;; add-hook
-  (add-hook! 'markdown-mode-hook 'auto-fill-mode)
-  (add-hook! 'org-mode-hook 'auto-fill-mode)
+  (add-hook 'markdown-mode-hook 'auto-fill-mode)
+  (add-hook 'org-mode-hook 'auto-fill-mode)
   )
 (message "MY-WORKFLOW-KR-LOADING... DONE")
+
 ;; Module: `me-notes' -- Package: `org-roam'
 (with-eval-after-load 'org-roam
-  (setq org-roam-directory "~/sync/org/roam/"
-        org-roam-db-location (concat org-roam-directory "org-roam.db"))
+  (setq org-roam-directory "~/sync/org/roam/")
 
   ;; Register capture template (via Org-Protocol)
   ;; Add this as bookmarklet in your browser
@@ -173,3 +306,5 @@
 ;;           ("Skyrock" . "http://icecast.skyrock.net/s/natio_mp3_128k"))
 ;;         ;; See https://docs.invidious.io/instances/
 ;;         empv-invidious-instance "https://invidious.projectsegfau.lt/api/v1"))
+
+(message "MY-MINEMACS-KR-LOADING... DONE")
---
+++ modules/me-org-ko.el
@@ -0,0 +1,299 @@
+;;; me-org-ko.el --- Org related stuff -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Abdelhak Bougouffa
+
+;; Author: Abdelhak Bougouffa <abougouffa@fedoraproject.org>
+
+(use-package org
+  :straight (:type built-in)
+  :defer (not (daemonp))
+  :preface
+  ;; Set to nil so we can detect user changes (in config.el)
+  (setq org-directory nil)
+  ;; :custom
+  ;; (org-tags-column 0)
+  ;; (org-auto-align-tags nil)
+  ;; (org-startup-indented nil)
+  ;; (org-cycle-hide-block-startup t)
+  ;; (org-return-follows-link t) ; RET follows link (a key bind has to be defined for Evil, see below)
+  ;; (org-fold-catch-invisible-edits 'smart) ; try not to accidently do weird stuff in invisible regions
+  ;; (org-fontify-quote-and-verse-blocks t)
+  ;; (org-special-ctrl-a/e t)
+  ;; (org-insert-heading-respect-content t)
+  ;; (org-hide-emphasis-markers t)
+  ;; (org-use-property-inheritance t) ; it's convenient to have properties inherited
+  ;; (org-ellipsis " ↩")
+  ;; (org-log-done 'time) ; having the time an item is done sounds convenient
+  ;; (org-list-allow-alphabetical t) ; have a. A. a) A) list bullets
+  ;; (org-export-in-background t) ; run export processes in external emacs process
+  ;; (org-export-async-init-file (expand-file-name (concat minemacs-modules-dir "extras/me-org-export-async-init.el")))
+  ;; (org-export-with-smart-quotes t) ; convert "this" to « this »
+  ;; (org-export-with-sub-superscripts '{}) ; Only explicit _{} ^{} are interpreted as sub/superscripts
+  ;; (org-highlight-latex-and-related '(native script entities))
+  ;; (org-pretty-entities t)
+  ;; (org-pretty-entities-include-sub-superscripts t)
+  ;; (org-use-sub-superscripts '{}) ; Do the same when rendering the Org buffer
+  ;; (org-edit-src-content-indentation 0) ; do not indent the content of src blocks
+  ;; (org-edit-src-turn-on-auto-save t) ; auto-save org-edit-src
+  ;; (org-edit-src-auto-save-idle-delay auto-save-timeout) ; use the defaults
+  :config
+  (+map-local :keymaps 'org-mode-map
+    "l"  '(nil :wk "link")
+    "ll" #'org-insert-link
+    "e"  #'org-export-dispatch
+    "s"  #'org-edit-src-code)
+  (+map-local :keymaps 'org-src-mode-map
+    "s" #'org-edit-src-save
+    "q" #'org-edit-src-abort
+    "e" #'org-edit-src-exit)
+  (+map-key
+    :keymaps 'org-mode-map
+    :states 'normal
+    "RET" #'org-open-at-point)
+
+  (setq org-export-async-debug minemacs-debug) ;; Can be useful!
+
+  ;; Dynamically change font size for Org heading levels, starting from
+  ;; `+org-level-base-size', and shrinking by a factor of 0.9 at each level.
+  ;; (defvar +org-level-base-size 1.3)
+
+  ;; (dotimes (level 8)
+  ;;   (let ((size (max 1.0 (* +org-level-base-size (expt 0.9 level)))))
+  ;;     (set-face-attribute
+  ;;      (intern (format "org-level-%d" (1+ level))) nil
+  ;;      :weight (cond ((< level 3) 'semi-bold)
+  ;;                    ((< level 5) 'bold)
+  ;;                    (t 'heavy))
+  ;;      :height size)))
+
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((C . t)
+     (R . t)
+     (js . t)
+     (dot . t)
+     (awk . t)
+     (sed . t)
+     (sql . t)
+     (org . t)
+     (shell . t)
+     (ditaa . t)
+     (latex . t)
+     (julia . t)
+     (sqlite . t)
+     (octave . t)
+     (maxima . t)
+     (eshell . t)
+     (scheme . t)
+     (python . t)
+     (clojure . t)
+     (gnuplot . t)
+     (plantuml . t)
+     (makefile . t)))
+
+  (with-eval-after-load 'org-src
+    (setq org-src-lang-modes
+          (append
+           '(("dot" . graphviz-dot))
+           (delete (assoc "dot" org-src-lang-modes #'equal) org-src-lang-modes))))
+
+  (with-eval-after-load 'plantuml-mode
+    (setq org-plantuml-jar-path plantuml-jar-path
+          org-plantuml-exec-mode plantuml-default-exec-mode
+          org-plantuml-executable-path plantuml-executable-path)))
+
+;; (use-package me-org-extras
+;;   :after org
+;;   :config
+;;   (+org-extras-outline-path-setup)
+;;   (+org-extras-pretty-latex-fragments-setup)
+;;   (+org-extras-latex-classes-setup)
+;;   (+org-extras-responsive-images-setup)
+;;   (+org-extras-equation-numbering-setup)
+;;   (+org-extras-multifiles-document-setup)
+;;   (+org-extras-lower-case-keywords-and-properties-setup))
+
+(use-package org-contrib
+  :straight t
+  :after org)
+
+;; (use-package engrave-faces
+;;   :straight t
+;;   :after org
+;;   :custom
+;;   (org-latex-src-block-backend 'engraved))
+
+;; ;; Org export
+;; (use-package ox-latex
+;;   :after org
+;;   :custom
+;;   (org-latex-prefer-user-labels t)
+;;   ;; Default `minted` options, can be overwritten in file/dir locals
+;;   (org-latex-minted-options
+;;    '(("frame"         "lines")
+;;      ("fontsize"      "\\footnotesize")
+;;      ("tabsize"       "2")
+;;      ("breaklines"    "true")
+;;      ("breakanywhere" "true") ;; break anywhere, no just on spaces
+;;      ("style"         "default")
+;;      ("bgcolor"       "GhostWhite")
+;;      ("linenos"       "true")))
+;;   :config
+;;   ;; Map some org-mode blocks' languages to lexers supported by minted
+;;   ;; you can see supported lexers by running this command in a terminal:
+;;   ;; 'pygmentize -L lexers'
+;;   (dolist (pair '((ipython    "python")
+;;                   (jupyter    "python")
+;;                   (scheme     "scheme")
+;;                   (lisp-data  "lisp")
+;;                   (conf-unix  "unixconfig")
+;;                   (conf-space "unixconfig")
+;;                   (authinfo   "unixconfig")
+;;                   (gdb-script "unixconfig")
+;;                   (conf-toml  "yaml")
+;;                   (conf       "ini")
+;;                   (gitconfig  "ini")
+;;                   (systemd    "ini")))
+;;     (unless (member pair org-latex-minted-langs)
+;;       (add-to-list 'org-latex-minted-langs pair))))
+
+(use-package ox-hugo
+  :straight t
+  :after org)
+
+;; (use-package ox-extra
+;;   :after org
+;;   :config
+;;   (ox-extras-activate '(latex-header-blocks ignore-headlines)))
+
+;; Org babel
+(use-package ob-tangle
+  :after org)
+
+;; ;; Other Org features
+;; (use-package org-appear
+;;   :straight t
+;;   :hook (org-mode . org-appear-mode)
+;;   :custom
+;;   (org-appear-inside-latex t)
+;;   (org-appear-autokeywords t)
+;;   (org-appear-autoentities t)
+;;   (org-appear-autoemphasis t)
+;;   (org-appear-autosubmarkers t)
+;;   (org-appear-autolinks 'just-brackets)
+;;   :config
+;;   ;; for proper first-time setup, `org-appear--set-elements'
+;;   ;; needs to be run after other hooks have acted.
+;;   (run-at-time nil nil #'org-appear--set-elements))
+
+;; (use-package org-modern
+;;   :straight t
+;;   :hook (org-mode . org-modern-mode)
+;;   :hook (org-agenda-finalize . org-modern-agenda)
+;;   :custom-face
+;;   ;; Force monospaced font for tags
+;;   (org-modern-tag ((t (:inherit org-verbatim :weight regular :foreground "black" :background "LightGray" :box "black"))))
+;;   :custom
+;;   (org-modern-star '("◉" "○" "◈" "◇" "✳" "◆" "✸" "▶"))
+;;   (org-modern-table-vertical 5)
+;;   (org-modern-table-horizontal 2)
+;;   (org-modern-list '((?+ . "➤") (?- . "–") (?* . "•")))
+;;   (org-modern-block-fringe nil)
+;;   (org-modern-todo-faces
+;;    ;; Tweak colors, and force it to be monospaced, useful when using
+;;    ;; mixed-pitch-mode.
+;;    '(("IDEA" . (:inherit org-verbatim :weight semi-bold
+;;                 :foreground "white" :background "goldenrod"))
+;;      ("NEXT" . (:inherit org-verbatim :weight semi-bold
+;;                 :foreground "white" :background "IndianRed1"))
+;;      ("STRT" . (:inherit org-verbatim :weight semi-bold
+;;                 :foreground "white" :background "OrangeRed"))
+;;      ("WAIT" . (:inherit org-verbatim :weight semi-bold
+;;                 :foreground "white" :background "coral"))
+;;      ("KILL" . (:inherit org-verbatim :weight semi-bold
+;;                 :foreground "white" :background "DarkGreen"))
+;;      ("PROJ" . (:inherit org-verbatim :weight semi-bold
+;;                 :foreground "white" :background "LimeGreen"))
+;;      ("HOLD" . (:inherit org-verbatim :weight semi-bold
+;;                 :foreground "white" :background "orange"))
+;;      ("DONE" . (:inherit org-verbatim :weight semi-bold
+;;                 :foreground "black" :background "LightGray")))))
+
+;; (use-package org-agenda
+;;   :straight (:type built-in)
+;;   :defer t
+;;   :custom
+;;   (org-agenda-tags-column 0)
+;;   (org-agenda-block-separator ?─)
+;;   (org-agenda-time-grid
+;;    '((daily today require-timed)
+;;      (800 1000 1200 1400 1600 1800 2000)
+;;      " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄"))
+;;   (org-agenda-current-time-string
+;;    "⭠ now ─────────────────────────────────────────────────"))
+
+;; ;; For latex fragments
+;; (use-package org-fragtog
+;;   :straight t
+;;   :hook (org-mode . org-fragtog-mode)
+;;   :custom
+;;   (org-fragtog-preview-delay 0.2))
+
+;; (use-package org-present
+;;   :straight t
+;;   :general
+;;   (+map "oP" :keymaps 'org-mode-map #'org-present)
+;;   :config
+;;   (setq org-present-text-scale 2.5)
+
+;;   (defvar-local +org-present--vcm-params
+;;       '(:enabled nil
+;;         :width nil
+;;         :center-text nil)
+;;     "Variable to hold `visual-fill-column-mode' parameters")
+
+;;   (add-hook
+;;    'org-present-mode-hook
+;;    (defun +org-present--on-h ()
+;;      (setq-local
+;;       face-remapping-alist
+;;       '((default (:height 1.5) variable-pitch)
+;;         (header-line (:height 2.0) variable-pitch)
+;;         (org-document-title (:height 2.0) org-document-title)
+;;         (org-code (:height 1.55) org-code)
+;;         (org-verbatim (:height 1.55) org-verbatim)
+;;         (org-block (:height 1.25) org-block)
+;;         (org-block-begin-line (:height 0.7) org-block)))
+;;      ;; (org-present-big)
+;;      (org-display-inline-images)
+;;      (org-present-hide-cursor)
+;;      (org-present-read-only)
+;;      (when (bound-and-true-p visual-fill-column-mode)
+;;        (+plist-push! +org-present--vcm-params
+;;          :enabled visual-fill-column-mode
+;;          :width visual-fill-column-width
+;;          :center-text visual-fill-column-center-text))
+;;      (setq-local visual-fill-column-width 120
+;;                  visual-fill-column-center-text t)
+;;      (visual-fill-column-mode 1)))
+
+;;   (add-hook
+;;    'org-present-mode-quit-hook
+;;    (defun +org-present--off-h ()
+;;      (setq-local
+;;       face-remapping-alist
+;;       '((default default default)))
+;;      ;; (org-present-small)
+;;      (org-remove-inline-images)
+;;      (org-present-show-cursor)
+;;      (org-present-read-write)
+;;      (visual-fill-column-mode -1)
+;;      (unless (plist-get +org-present--vcm-params :enabled)
+;;        (setq-local visual-fill-column-width (plist-get +org-present--vcm-params :width)
+;;                    visual-fill-column-center-text (plist-get +org-present--vcm-params :center-text))
+;;        (visual-fill-column-mode 1)))))
+
+
+(provide 'me-org-ko)
+
+;;; me-org-ko.el ends here
